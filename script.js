18ade2a452921bc1710e09df2194667dc87503b9
Marcos-Vitor123 <50049290+Marcos-Vitor123@users.noreply.github.com>
2022-06-12 13:52:51 -0300

    Ajustes finais da aplicação

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const buttonPesquisar = document.querySelector("#buttonPesquisar")
 inputPesquisaGoogle.value = ""
 
 function fazerPesquisaGoogle() {
+  elementoRespostaGoogle.style.opacity = 0
 
   if(inputPesquisaGoogle.value == "") {
+    const pesquisar = "<div>" + inputPesquisaGoogle.value + "</div>"
+    elementoRespostaGoogle.innerHTML = pesquisar
     elementoRespostaGoogle.style.color = "yellow"
     elementoRespostaGoogle.innerHTML = "Digite a sua pesquisa!"
     setTimeout(function(){
@@ -15,15 +18,12 @@ function fazerPesquisaGoogle() {
   }
 
   inputPesquisaGoogle.setAttribute("disabled", true)
-  const pesquisar = "<div>" + inputPesquisaGoogle.value + "</div>"
-  elementoRespostaGoogle.innerHTML = pesquisar
   const pesquisaGoogle = `https://www.google.com/search?q=${inputPesquisaGoogle.value}`
 
   window.location.href = pesquisaGoogle
   inputPesquisaGoogle.value = ""
 
   setTimeout(function() {
-    elementoRespostaGoogle.style.opacity = 0
     buttonPesquisar.removeAttribute("disabled")
   }, 3000)
 }
